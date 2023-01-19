855d5e84a690577bdcda7ca6eba5ccdf270d0efa
Renan Carvalho <rcrvlh90@gmail.com>
2023-01-19 12:15:07 -0300

    add smooth transition
    Added smooth transition to the page with jQuery.

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-document.querySelectorAll('a[href^="."]').forEach(anchor => {
+/*document.querySelectorAll('a[href^="."]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault()
 
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         })
+    })
+})*/
+
+$(document).ready(function() {
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault()
+
+            var hash = this.hash
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+                window.location.hash = hash
+            })
+        }
     })
 })
